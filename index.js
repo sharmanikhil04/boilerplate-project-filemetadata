c3c13f5b15e918b89164cba1581b4d464f5ebc96
Nikhil Sharma <nikhil.sharma@tietoevry.com>
2024-10-01 17:41:36 +0530

    File upload

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
-
+var multer = require('multer');
+const uploadStorage = multer({ dest: 'uploads/' })
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -18,3 +19,17 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse',uploadStorage.single("upfile"), (req,res) =>{
+  return res.json({"name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size})
+})
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/")
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname)
+  },
+})
